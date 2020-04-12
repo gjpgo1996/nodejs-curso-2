9e8a2aa047711436ece953e3812660c7b72821e2
gjpgo1996 <gjpgot@gmail.com>
2020-04-12 21:56:56 +0000

    probando modulos de nodejs del os

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-const os = require('os');
+/* const os = require('os');
 
 console.log(os.platform());
+console.log(os.release());
+console.log('free mem: ', os.freemem(), ' bytes');
+console.log('total mem: ', os.totalmem(), ' bytes'); */
+
+/*const fs = require('fs');
+
+fs.writeFile('./texto.txt', 'línea uno', function (err) {
+    if (err) {
+        console.log(err);
+    }
+        console.log('archivo creado');
+});
+
+console.log('ultima linea de código');
+
+*/
+
+/*
+const fs = require('fs');
+
+fs.readFile('./texto.txt', function(err, data) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(data.toString());
+}) */
+
+const http = require('http');
+
+const handleServer = function(req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.write('<h1>Hola Mundo</h1>');
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function() {
+    console.log('Server on port 3000');
+});
