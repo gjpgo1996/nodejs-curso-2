38795a196cc41b5ba18ff40b1739d97ff01b6d88
gjpgo1996 <gjpgot@gmail.com>
2020-04-12 22:25:48 +0000

    servidor con nodejs y express

--- index.js
+++ index.js
@@ -28,7 +28,11 @@ fs.readFile('./texto.txt', function(err, data) {
     console.log(data.toString());
 }) */
 
+
+/*
 const http = require('http');
+
+const colors = require('colors');
 
 const handleServer = function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
@@ -39,5 +43,21 @@ const handleServer = function(req, res) {
 const server = http.createServer(handleServer);
 
 server.listen(3000, function() {
-    console.log('Server on port 3000');
+    console.log('Server on port 3000'.yellow);
+});
+
+*/
+
+const express = require('express');
+const colors = require('colors');
+
+const server = express();
+
+server.get('/', function (req, res) {
+    res.send('<h1>Hola Mundo con Express y Nodejs</h1>');
+    res.end();
+});
+
+server.listen(3000, function () {
+    console.log('Server on port 3000'.red);
 });
